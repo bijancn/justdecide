9a8744cb493c236a6e4c471e9135c6e1988525f2
Bijan Chokoufe Nejad <2117164+bijancn@users.noreply.github.com>
2021-04-14 11:01:05 +0200

    feat: VOT-20: Add options to a topic

--- pages/topics/[id].js
+++ pages/topics/[id].js
@@ -3,15 +3,16 @@ import React, { useEffect, useState } from "react";
 import TopicOptions from "../../components/TopicOptions";
 import { fetchTopic } from "../../lib/TopicsDao";
 
-const TopicDetailPage = () => {
+const TopicDetailPage = (props) => {
   const router = useRouter();
   const { id } = router.query;
-  const [topic, setTopic] = useState({ description: "" });
-  useEffect(() => {
+  const [topic, setTopic] = useState({ title: "" });
+  useEffect(async () => {
     if (!router.isReady) return;
-    fetchTopic(setTopic, id);
+    const topic = await fetchTopic(id);
+    setTopic(topic);
   }, [router.isReady]);
-  return <TopicOptions topicDescription={topic.description} />;
+  return <TopicOptions topic={topic} userId={props.user.id} />;
 };
 
 export default TopicDetailPage;
