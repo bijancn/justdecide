1dbf51725b6d07e657f6897f5699ab9daea5090b
Bijan Chokoufe Nejad <2117164+bijancn@users.noreply.github.com>
2021-04-14 11:01:05 +0200

    feat: VOT-20: Add options to a topic

--- components/TopicOptions.tsx
+++ components/TopicOptions.tsx
@@ -1,51 +1,57 @@
-import { useRouter } from "next/router";
-import { fetchTopic } from "../lib/TopicsDao";
 import {
-  Heading,
-  FormControl,
-  Input,
-  Center,
   Box,
-  FormErrorMessage,
   Button,
+  Center,
+  FormControl,
+  FormErrorMessage,
+  Heading,
+  Input,
   Text,
 } from "@chakra-ui/react";
-import { Formik, Field, Form, ErrorMessage, FieldArray } from "formik";
-import React, { useState, useEffect } from "react";
+import { Field, FieldArray, Form, Formik } from "formik";
+import { addOptions } from "../lib/TopicsDao";
+import React from "react";
 
 const initialValues = {
   options: [
     {
-      description: "",
+      title: "",
     },
     {
-      description: "",
+      title: "",
     },
   ],
 };
 
-const sendOptions = async (values) => {
+const sendOptions = async (userId, topicId, values) => {
   await new Promise((r) => setTimeout(r, 500));
-  alert(JSON.stringify(values, null, 2));
+  const result = await addOptions(userId, topicId, values);
+  alert(JSON.stringify(result));
 };
 
-interface TopicOptionsProp {
-  topicDescription: string;
+interface Topic {
+  title: string;
+  id: string;
 }
 
-export default function TopicOptions(prop: TopicOptionsProp) {
+interface TopicOptionsProps {
+  topic: Topic;
+  userId: string;
+}
+
+export default function TopicOptions(props: TopicOptionsProps) {
   return (
     <>
       <Box boxShadow="" p="6" rounded="md" bg="white">
         <Heading fontSize={{ base: "xl", sm: "2xl", md: "4xl" }}>
           <p>Possible options for</p>
-          <Text color="#e53e3e">{prop.topicDescription}</Text>
+          <Text color="#e53e3e">{props.topic.title}</Text>
           <p>are</p>
         </Heading>
         <Formik
           initialValues={initialValues}
           onSubmit={(formValue, actions) => {
-            sendOptions(formValue.options);
+            sendOptions(props.userId, props.topic.id, formValue.options);
             actions.setSubmitting(false);
           }}
         >
@@ -57,7 +63,7 @@ export default function TopicOptions(prop: TopicOptionsProp) {
                     {props.values.options.length > 0 &&
                       props.values.options.map((friend, index) => (
                         <div className="row" key={index}>
-                          <Field name={`options.${index}.description`}>
+                          <Field name={`options.${index}.title`}>
                             {({ field, form }) => (
                               <FormControl>
                                 <Input
@@ -82,10 +88,10 @@ export default function TopicOptions(prop: TopicOptionsProp) {
                         </div>
                       ))}
                     {props.values.options.every(
-                      (value, index) => value.description.length > 0
+                      (value, index) => value.title.length > 0
                     ) &&
                       push({
-                        description: "",
+                        title: "",
                       })}
                   </div>
                 )}
