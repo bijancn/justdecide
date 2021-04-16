350dc4e0d3f8de7248136258aba419071d410ad0
Bijan Chokoufe Nejad <2117164+bijancn@users.noreply.github.com>
2021-04-16 13:18:52 +0200

    feat: Implement veto/block and like slider
    Fixes #10

--- lib/initSupabase.ts
+++ lib/initSupabase.ts
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
+export const supabase: SupabaseClient = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
